fa0a7207408a993dfbdee3a3898b4016c69c8c17
Graham Fairweather <xotic750@gmail.com>
2019-08-13 14:25:51 +0200

    :bookmark: v1.0.0

--- dist/global-this-x.js
+++ dist/global-this-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2019-present",
-  "date": "2019-08-13T12:17:17.405Z",
+  "date": "2019-08-13T12:25:32.907Z",
   "describe": "",
   "description": "Get globalThis.",
   "file": "global-this-x.js",
